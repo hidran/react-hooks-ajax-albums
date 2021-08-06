5b8894a3d5775a12434c5aeacdc3c695ca32503b
Hidran Arias <hidran@gmail.com>
2021-08-06 13:16:55 +0200

    adds useState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [photos, setPhotos] = useState([]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>ALBUMS</h1>
+        <ul>
+          {
+            photos.map(photo =>
+              <li key={photo.id}>
+              <img src={photo.thumbnailUrl} title={photo.title} alt={photo.title}/></li>
+              )
+          }
+        </ul>
       </header>
     </div>
   );
