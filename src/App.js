cddf92d5ec4b94cb314428a62988651b74e07758
Hidran Arias <hidran@gmail.com>
2021-08-06 13:34:14 +0200

    adds useEffect and fetches photos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const photosUrl = 'https://jsonplaceholder.typicode.com/photos';
+
+    const albumsUrl = 'https://jsonplaceholder.typicode.com/albums';
+
+    const usersUrl = 'https://jsonplaceholder.typicode.com/users';
+
+
   const [photos, setPhotos] = useState([]);
+  const [albums, setAlbums] = useState([]);
+    const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const getPhotos = async () => {
+      const photos = await fetch(photosUrl).then(res => res.json());
+      setPhotos(photos.slice(0,20));
+    };
+    getPhotos();
+
+    return () => {
+      
+    }
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <h1>ALBUMS</h1>
-        <ul>
+        <ul className="photos">
           {
             photos.map(photo =>
               <li key={photo.id}>
