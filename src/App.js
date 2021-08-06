802c58c35cf31bd432f21104f722797def357b15
Hidran Arias <hidran@gmail.com>
2021-08-06 13:49:17 +0200

    adds useEffect and useState for albums and users

--- src/App.js
+++ src/App.js
@@ -12,22 +12,73 @@ function App() {
 
   const [photos, setPhotos] = useState([]);
   const [albums, setAlbums] = useState([]);
-    const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
+  
   useEffect(() => {
     const getPhotos = async () => {
       const photos = await fetch(photosUrl).then(res => res.json());
-      setPhotos(photos.slice(0,20));
+      setPhotos(photos.slice(0, 20));
     };
     getPhotos();
 
     return () => {
       
     }
-  }, [])
+  }, []);
+  
+  useEffect(() => {
+    const getAlbums = async () => {
+      const albums = await fetch(albumsUrl).then(res => res.json());
+      setAlbums(albums.slice(0, 20));
+    };
+    getAlbums();
+
+    return () => {
+      
+    }
+  }, []);
+  useEffect(() => {
+    const getUsers = async () => {
+      const users = await fetch(usersUrl).then(res => res.json());
+      setUsers(users.slice(0, 20));
+    };
+    getUsers();
+
+    return () => {
+      
+    }
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <h1>ALBUMS</h1>
+         <form className="gallery">
+          <div className="form-group">
+        <label htmlFor="users"> USERS
+             <select name ="users" id="users">
+             <option value="">SELECT</option>
+               {
+                  users.map(a => <option key={a.id} value={a.id}>{ a.name}</option>)
+                 
+               }
+             </select>
+
+          </label>
+          </div>
+         <div className="form-group">
+          <label htmlFor="albums"> ALBUMS
+             <select name ="albums" id="albums">
+             <option value="">SELECT</option>
+               {
+                  albums.map(a => <option key={a.id} value={a.id}>{ a.title}</option>)
+                 
+               }
+             </select>
+
+          </label>
+          
+        </div>
+        </form>
         <ul className="photos">
           {
             photos.map(photo =>
